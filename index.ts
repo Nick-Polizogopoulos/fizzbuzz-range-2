739710d27fdd90ce58a08979176ccc56ac49f1e3
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-06-28 15:41:33 +0300

    index.ts
    I fixed the names of the variables to be more friendly.

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 firstRange(20, 30);
 secondRange(60, 70);
 
-function check (x: number):string {
-    const t: boolean = x % 3 === 0, f: boolean = x % 5 === 0;
-    return t && f ? 'FizzBuzz' : 
-                t ? 'Fizz' :
-                f ? 'Buzz' : 
+function check (number:number):string {
+    const three: boolean = number % 3 === 0, five: boolean = number % 5 === 0;
+    return three && five ? 'FizzBuzz' : 
+                three ? 'Fizz' :
+                five ? 'Buzz' : 
                 '-';
 }
-function firstRange (one:number, two: number): void {
+function firstRange (firstNumber:number, secondNumber: number): void {
     console.log(`First Range of Numbers:
 -----------------------`);
-    for (let i:number = one; i <= two; i++) console.log(`${i}: ${check(i)}`);
+    for (let i:number = firstNumber; i <= secondNumber; i++) console.log(`${i}: ${check(i)}`);
     console.log('');
 }
-function secondRange (one:number, two: number): void {
+function secondRange (firstNumber:number, secondNumber: number): void {
     console.log(`Second Range of Numbers:
 ------------------------`);
-    for (let i:number = one; i <= two; i++)console.log(`${i}: ${check(i)}`);
+    for (let i:number = firstNumber; i <= secondNumber; i++)console.log(`${i}: ${check(i)}`);
     console.log('');
 }
 
