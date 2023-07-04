def6a2051e77467085e66d6b38cc9e99d991c931
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-07-04 05:38:57 +0300

    index.ts

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ secondRange(60, 70);
 
 function check (number:number):string {
     const three: boolean = number % 3 === 0, five: boolean = number % 5 === 0;
-    return three && five ? 'FizzBuzz' : 
-                three ? 'Fizz' :
-                five ? 'Buzz' : 
-                '-';
+    return three && five ? 'FizzBuzz'
+                         : three ? 'Fizz'
+                         : five ? 'Buzz'
+                         : '-';
 }
 function firstRange (firstNumber:number, secondNumber: number): void {
     console.log(`First Range of Numbers:
